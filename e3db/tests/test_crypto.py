e5f55f5ab579667d2e169c633e2d358bd58e9586
Ben Reichert <ben@tozny.com>
2017-10-09 13:22:07 -0700

    Fleshed out crypto tests.

--- e3db/tests/test_crypto.py
+++ e3db/tests/test_crypto.py
@@ -11,6 +11,20 @@ def test_base64():
     assert(string == decoded)
 
 def test_generate_keypair():
-    privkey, pubkey = e3db.Crypto.generate_keypair()
+    pubkey, privkey = e3db.Crypto.generate_keypair()
     assert(type(privkey) == nacl.public.PrivateKey)
     assert(type(pubkey) == nacl.public.PublicKey)
+
+def test_public_key_encoding():
+    pubkey = 'RMG04iil2HDaUWye9wMVG8RmIL_s5tPOilRoiLUjLT8'
+    decoded = e3db.Crypto.decode_public_key(pubkey)
+    encoded = e3db.Crypto.encode_public_key(decoded)
+    assert(pubkey == encoded)
+    assert(type(decoded) == nacl.public.PublicKey)
+
+def test_private_key_encoding():
+    privkey = '_wSGC32a3g_VOPPy3kILDqzKLa1tPwdTNW3DQrJMPxk'
+    decoded = e3db.Crypto.decode_private_key(privkey)
+    encoded = e3db.Crypto.encode_private_key(decoded)
+    assert(privkey == encoded)
+    assert(type(decoded) == nacl.public.PrivateKey)

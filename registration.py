1720992c0862caaef41e22c08db9d0b3964df26c
Ben Reichert <ben@tozny.com>
2017-10-18 13:25:07 -0700

    Update registration example with new getters/setters and remove old TODOs.

--- registration.py
+++ registration.py
@@ -14,14 +14,13 @@
 
 import e3db
 
-# required to generate random client id
+# Required to generate random client id used in this example
 import os
 import binascii
 
 # A registration token is required to set up a client. In this situation,
 # we assume an environment variable called REGISTRATION_TOKEN is set
 token = os.environ["REGISTRATION_TOKEN"]
-api_url = os.environ["DEFAULT_API_URL"] # TODO REMOVE
 
 print "Using Registration Token: {0}".format(token)
 
@@ -39,7 +38,6 @@ print "Client Name: {0}".format(client_name)
 
 # Passing all of the data above into the registration routine will create
 # a new client with the system. Remember to keep your private key private!
-#client_info = e3db.Client.register(token, client_name, public_key, api_url=api_url) # TODO remove api_url
 
 # Optionally, you can automatically back up the credentials of the newly-created
 # client to your InnoVault account (accessible via https://console.tozny.com) by
@@ -48,11 +46,14 @@ print "Client Name: {0}".format(client_name)
 # copy of its credentials that is itself stored in e3db for later use.
 #
 # Client credentials are not backed up by default.
+#
+# client_info = e3db.Client.register(token, client_name, public_key, private_key=private_key, backup=True)
 
-client_info = e3db.Client.register(token, client_name, public_key, private_key=private_key, backup=True, api_url=api_url)
+client_info = e3db.Client.register(token, client_name, public_key)
 
-api_key_id, api_secret = client_info.get_api_credentials()
-client_id = client_info.get_client_id()
+api_key_id = client_info.api_key_id
+api_secret = client_info.api_secret
+client_id = client_info.client_id
 
 print "Client ID: {0}".format(client_id)
 print "API Key ID: {0}".format(api_key_id)
@@ -71,8 +72,7 @@ config = e3db.Config(
     api_key_id, \
     api_secret, \
     public_key, \
-    private_key, \
-    api_url=api_url \
+    private_key
     )
 
 # Now create a client using that configuration.

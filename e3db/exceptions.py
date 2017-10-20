22a47aff53ccf4a38d52f56eb0d53496c7903d5c
Ben Reichert <ben@tozny.com>
2017-10-20 13:40:55 -0700

    Inherit from APIError.

--- e3db/exceptions.py
+++ e3db/exceptions.py
@@ -1,19 +1,19 @@
-class QueryError(Exception):
+class APIError(Exception):
+    def __init__(self, text):
+        Exception.__init__(self, "Error during API operation: {0}".format(text))
+
+class QueryError(APIError):
     def __init__(self, record_id):
-        Exception.__init__(self, "Error during query: {0}".format(record_id))
+        APIError.__init__(self, "Error during query: {0}".format(record_id))
 
-class LookupError(Exception):
+class LookupError(APIError):
     def __init__(self, text):
-        Exception.__init__(self, "Error during lookup: {0}".format(text))
+        APIError.__init__(self, "Error during lookup: {0}".format(text))
 
-class ConflictError(Exception):
+class ConflictError(APIError):
     def __init__(self, text):
-        Exception.__init__(self, "Conflict error: {0}".format(text))
+        APIError.__init__(self, "Conflict error: {0}".format(text))
 
 class CryptoError(Exception):
     def __init__(self, text):
         Exception.__init__(self, "Error during Crypto operation: {0}".format(text))
-
-class APIError(Exception):
-    def __init__(self, text):
-        Exception.__init__(self, "Error during API operation: {0}".format(text))

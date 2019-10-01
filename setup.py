8a61edb714003f57d97179e5526c182dcc42248d
Eli Fabens <efabens@tozny.com>
2019-10-01 08:51:11 -0700

    Update to setup.py to pass pypi changes

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@ from setuptools import setup, find_packages
 version = "2.1.4"
 setup(
   name="e3db",
+  description="E3DB provides a familiar JSON-based NoSQL-style API for reading, writing, and querying end-to-end encrypted data in the cloud.",
+  long_description_content_type='text/markdown',
+  long_description=open("README.md").read(),
   version=version,
   packages=find_packages(),
   install_requires=[
@@ -11,10 +14,9 @@ setup(
   ],
   url = "https://github.com/tozny/e3db-python",
   download_url = 'https://github.com/tozny/e3db-python/archive/{0}.tar.gz'.format(version),
-  description  = open("README.md").read(),
   author = "Tozny, LLC",
   author_email = "info@tozny.com",
-  license = open("LICENSE.md").read(),
-  keywords = ['e3db', 'encryption', 'encrypted-store'],
+  license = "TOZNY NON-COMMERCIAL LICENSE",
+  keywords = ['e3db', 'encryption', 'encrypted-store', 'tozstore'],
   classifiers = [],
 )
